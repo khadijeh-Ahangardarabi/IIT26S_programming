c30239b0ad88fe65d96399f03f42b1cf33a283e6
khadijeh-Ahangardarabi <khadijeh.ahangardarabi.96@gmail.com>
2026-09-19 17:24:30 +0300

    Remove integer input and remainder calculation
    Removed code that prompts for an integer and calculates the remainder.

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-print("Insert an integer: ")
 
-Feed = input()
-Value = int(Feed)
-
-Remainder = Value % 2
-
-print(f"Value is {Value}")
-print(f"The remainder is {Remainder} when {Value} is divided by 2.")
